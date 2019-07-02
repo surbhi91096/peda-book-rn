39cab0d70a002a507aa4ebc8f8dd40de9acdff74
Mustafa Vohra <mustafa@desiredsoft.net>
2019-07-02 10:46:36 +0530

    Milestone 4 completed

--- android/app/src/main/java/com/pedabook/MainApplication.java
+++ android/app/src/main/java/com/pedabook/MainApplication.java
@@ -3,6 +3,8 @@ package com.pedabook;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.wonday.pdf.RCTPdfView;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTPdfView(),
+            new RNFetchBlobPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage()
